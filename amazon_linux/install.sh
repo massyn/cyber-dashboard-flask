ccab7f1c96901f6dc6c0fc4ba6376a98afe9c303
Phil Massyn <phil.massyn@icloud.com>
2025-01-31 19:44:55 +1000

    Update

--- amazon_linux/install.sh
+++ amazon_linux/install.sh
@@ -5,3 +5,8 @@ chown ec2-user /cyber-dashboard-flask -R
 cp flaskapp.service /etc/systemd/system/flaskapp.service
 sudo systemctl start flaskapp
 sudo systemctl enable flaskapp
+
+sudo amazon-linux-extras install nginx1
+cp nginx.conf /etc/nginx/conf.d/app.conf
+sudo systemctl start nginx
+sudo systemctl enable nginx
